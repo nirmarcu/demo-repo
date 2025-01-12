b3e87197e4a612fd4d0cd9ce878ed4cae281635e
Nir Marcu <nirma@comm-it.com>
2025-01-12 12:21:00 +0200

    add basic caclculator operations

---
+++ calc.py
@@ -0,0 +1,14 @@
+# this is a basic calculator program, supporting basic operations like addition, subtraction, multiplication and division
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+    return x / y
